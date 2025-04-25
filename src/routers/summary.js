fe6332b03ca3a4abc5889ef2025b638ff2875f4a
BulatovaTati <kohodatat@gmail.com>
2025-04-25 15:40:08 +0300

    build docs

--- src/routers/summary.js
+++ src/routers/summary.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import { isAuthenticated } from '../middlewares/authMiddleware.js';
-import { getSummary } from '../controllers/summary.js';
 
 const summaryRouter = Router();
 
 // Отримати звітність за період
 //moneydashboard-back.onrender.com/api/transactions-summary
-summaryRouter.get('/', isAuthenticated, getSummary);
+summaryRouter.get('/');
 
 export default summaryRouter;
-
-
-
-
