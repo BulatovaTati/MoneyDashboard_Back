fcdb65a5c743b42b2843a0604ccf8fca17059a61
BulatovaTati <kohodatat@gmail.com>
2025-04-27 18:12:08 +0300

    add swagger corrections

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -9,22 +9,19 @@ import {
 import { validateBody } from '../middlewares/validateBody.js';
 
 const AuthRouter = Router();
-//moneydashboard-back.onrender.com/api/auth/sign-up
-// POST /auth/sign-up - реєстрація нового користувача
+
 AuthRouter.post(
   '/sign-up',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
 );
-//moneydashboard-back.onrender.com/api/auth/sign-in
-// POST /auth/sign-in - вхід користувача
+
 AuthRouter.post(
   '/sign-in',
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController),
 );
 
-// POST /auth/sign-out - вихід користувача
-AuthRouter.post('/sign-out', ctrlWrapper(logoutUserController)); // Визов методу в контролері
+AuthRouter.post('/sign-out', ctrlWrapper(logoutUserController));
 
 export default AuthRouter;
